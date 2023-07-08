c0af4bd57cc450e48498e59bd49600a58b6f133e
ykcsoftwarer <yusufkucukcopur@gmail.com>
2023-07-08 23:49:06 +0300

    change

--- src/components/footer/FooterPage.jsx
+++ src/components/footer/FooterPage.jsx
@@ -16,7 +16,8 @@ const FooterPage = () => {
             </div>
         
           </Col>
-          <Col md={3}>
+          <Col md={3} className="p-4 d-flex flex-column justify-content-center align-items-center ">
+            <div className="card-body w-100 text-start">
             <h5>Navigations</h5>
             <ul className="list-unstyled">
               <li>
@@ -35,8 +36,10 @@ const FooterPage = () => {
                 <a href="#blog">Blog</a>
               </li>
             </ul>
+            </div>
           </Col>
-          <Col md={6}>
+          <Col md={6} className="p-4 d-flex flex-column justify-content-center align-items-center ">
+            <div className="card-body w-100 text-start">
             <h5>My Account</h5>
             <ul  className="list-unstyled">
 
@@ -51,6 +54,7 @@ const FooterPage = () => {
                 <a href="#favorites">Favorites</a>
               </li>
         </ul>
+        </div>
           </Col>
         </Row>
       </Container>
