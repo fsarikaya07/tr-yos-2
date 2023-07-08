3dea869201ed6d92fa6ff1a61a3fcca05d5eb793
ykcsoftwarer <yusufkucukcopur@gmail.com>
2023-07-08 15:33:31 +0300

    v1

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Detail from "../components/Detail/Detail";
-import Home from "../page/Home";
+import Home from "../pages/Home";
 import Account from "../components/Account/Account";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
