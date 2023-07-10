b73593766846f0940148a27ea350cca8d24e8967
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-07-10 16:14:15 +0300

    helper folder added

---
+++ src/helper/data.jsx
@@ -0,0 +1,121 @@
+export const data = [
+    {
+      id:0,
+      title: "Turkish Folklore 10% Scholarship Faculity Of Pharmacy ",
+      desc:
+        "AFYON KOCATEPE UNIVERSITY",
+        cities:"Afyon",
+        years:"2500/year",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_1280.jpg",
+    },
+    {
+      id:1,
+      title: "Social Anthropology Faculity Of Pharmacy",
+      desc:
+        "ADIYAMAN UNIVERSITY",
+        cities:"Ankara",
+        years:"1500/year",
+      image:
+        "https://cdn.pixabay.com/photo/2016/02/05/16/19/offices-1181385_960_720.jpg",
+    },
+    {
+      id:2,
+      title: "Biology Faculty Of Arts And Sciences",
+      desc:
+        "ABDULLAH GÜL UNIVERSITY",
+        cities:"Kayseri",
+        years:"3500/year",
+      image:"https://cdn.pixabay.com/photo/2015/01/09/11/11/seminar-594125_1280.jpg",
+    },
+    {
+      id:3,
+      title: "Medical Engineering 25% Scholarship Medical School",
+      desc:
+        "ACIBADEM MEHMET ALI AYDINLAR UNIVERSITY",
+        cities:"İstanbul",
+        years:"0/year",
+      image:
+        "https://cdn.pixabay.com/photo/2014/08/02/11/37/books-408220_1280.jpg",
+    },
+    {
+      id:4,
+      title: "Turkish Folklore 10% Scholarship Faculity Of Pharmacy ",
+      desc:
+        "AFYON KOCATEPE UNIVERSITY",
+        cities:"Afyon",
+        years:"5800/year",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_1280.jpg",
+    },
+    {
+      id:5,
+      title: "Social Anthropology Faculity Of Pharmacy",
+      desc:
+        "ADIYAMAN UNIVERSITY",
+        cities:"Ankara",
+        years:"11500/year",
+      image:
+        "https://cdn.pixabay.com/photo/2016/02/05/16/19/offices-1181385_960_720.jpg",
+    },
+    {
+      id:6,
+      title: "Biology Faculty Of Arts And Sciences",
+      desc:
+        "ABDULLAH GÜL UNIVERSITY",
+        cities:"Kayseri",
+        years:"14000/year",
+      image:"https://cdn.pixabay.com/photo/2015/01/09/11/11/seminar-594125_1280.jpg",
+    },
+    {
+      id:7,
+      title: "Medical Engineering 25% Scholarship Medical School",
+      desc:
+        "ACIBADEM MEHMET ALI AYDINLAR UNIVERSITY",
+        cities:"İstanbul",
+        years:"2500/year",
+      image:
+        "https://cdn.pixabay.com/photo/2014/08/02/11/37/books-408220_1280.jpg",
+    },
+    {
+      id:8,
+      title: "Turkish Folklore 10% Scholarship Faculity Of Pharmacy ",
+      desc:
+        "AFYON KOCATEPE UNIVERSITY",
+        cities:"Afyon",
+        years:"2500/year",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_1280.jpg",
+    },
+    {
+      id:9,
+      title: "Social Anthropology Faculity Of Pharmacy",
+      desc:
+        "ADIYAMAN UNIVERSITY",
+        cities:"Ankara",
+        years:"2500/year",
+      image:
+        "https://cdn.pixabay.com/photo/2016/02/05/16/19/offices-1181385_960_720.jpg",
+    },
+    {
+      id:10,
+      title: "Biology Faculty Of Arts And Sciences",
+      desc:
+        "ABDULLAH GÜL UNIVERSITY",
+        cities:"Kayseri",
+        years:"2500/year",
+      image:"https://cdn.pixabay.com/photo/2015/01/09/11/11/seminar-594125_1280.jpg",
+    },
+    {
+      id:11,
+      title: "Medical Engineering 25% Scholarship Medical School",
+      desc:
+        "ACIBADEM MEHMET ALI AYDINLAR UNIVERSITY",
+        cities:"İstanbul",
+        years:"2500/year",
+      image:
+        "https://cdn.pixabay.com/photo/2014/08/02/11/37/books-408220_1280.jpg",
+    },
+    
+   
+  ];
