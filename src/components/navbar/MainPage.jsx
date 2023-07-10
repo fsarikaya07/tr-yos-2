61278b5fd89321905e17cc1efd82b446d16ae821
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-07-10 18:02:40 +0300

    humeyra

--- src/components/navbar/MainPage.jsx
+++ src/components/navbar/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { NavLink, Button, Container, Nav } from "react-bootstrap";
+import {  Button, Container, Nav } from "react-bootstrap";
+import {  NavLink } from "react-router-dom";
 
 const MainPage = () => {
   return (
