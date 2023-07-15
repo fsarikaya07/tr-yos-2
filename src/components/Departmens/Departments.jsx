e7224aa6cac9be08721b59e5a441fa995bd10610
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-07-15 18:35:35 +0300

    Depertments Updated kubra

--- src/components/Departmens/Departments.jsx
+++ src/components/Departmens/Departments.jsx
@@ -138,7 +138,19 @@ const Departments = () => {
                 </Button>
               </Form>
             </Col>
-           
+            <Col xs={12} sm={12} md={12} lg={8} xl={9}>
+              <Container className="rounded-4 mt-2 p-4">
+                <Row className="g-3 d-flex flex-wrap">
+                  {random12Cards?.map((item, index) => {
+                    return (
+                      <Col sm={6} md={6} lg={6} key={item.id}>
+                        <DepertmentsCard item={item} />
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </Container>
+            </Col>
           </div>
         </Row>
       </Container>
