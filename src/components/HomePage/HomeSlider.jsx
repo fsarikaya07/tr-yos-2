0b18699ab8c57896793c0d413b92b1d83ac8071e
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-07-18 14:54:01 +0300

    HomeSearcSlider Added

---
+++ src/components/HomePage/HomeSlider.jsx
@@ -0,0 +1,81 @@
+const HomeSlider = () => {
+    return (
+   
+  <Zoom
+    indicators
+   
+  
+    scale={1.4}
+    prevArrow={
+      <div className="icon-wrapper">
+        <div className="icon-container">
+          <i className="icon fas fa-chevron-left"></i>
+        </div>
+      </div>
+    }
+    nextArrow={
+      <div className="icon-wrapper">
+        <div className="icon-container">
+          <i className="icon fas fa-chevron-right"></i>
+        </div>
+      </div>
+        
+     
+    }
+    
+  >
+    
+        <div
+          className="slide-image"
+          style={{
+            backgroundImage:
+              "url(https://images.pexels.com/photos/3762800/pexels-photo-3762800.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            minHeight: "100vh",
+          }}
+        ></div>
+        <div>
+          <div
+            className="slide-image"
+            style={{
+              backgroundImage:
+                "url(https://images.pexels.com/photos/5940838/pexels-photo-5940838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center center",
+              minHeight: "100vh",
+            }}
+          ></div>
+        </div>
+  
+        <div
+          className="slide-image"
+          style={{
+            backgroundImage:
+            "url(https://images.pexels.com/photos/8199562/pexels-photo-8199562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            minHeight: "100vh",
+          }}
+        ></div>
+              <div
+          className="slide-image"
+          style={{
+            backgroundImage:
+            "url(https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            minHeight: "100vh",
+          }}
+        ></div>
+  
+      </Zoom>
+    );
+  };
+  
+  export default HomeSlider;
+  
