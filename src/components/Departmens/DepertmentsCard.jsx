bfd0e479f0f3b73524f56de7df9e061d93789250
ykcsoftwarer <yusufkucukcopur@gmail.com>
2023-07-19 17:05:13 +0300

    modal add and changes

--- src/components/Departmens/DepertmentsCard.jsx
+++ src/components/Departmens/DepertmentsCard.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import "../Style/Departmants.css";
-import { Link } from "react-router-dom";
+
 const DepertmentsCard = ({ image, title, desc, cities, years }) => {
+  // State değerleri ve toggle fonksiyonları tanımlanıyor
+  const [showSignInCompareModal, setShowSignInCompareModal] = useState(false);
+
+  const toggleShowSignInCompareModal = () =>
+    setShowSignInCompareModal(!showSignInCompareModal);
+  const [showSignInHeartModal, setShowSignInHeartModal] = useState(false);
+
+  const toggleShowSignInHeartModal = () =>
+    setShowSignInHeartModal(!showSignInHeartModal);
+
   return (
     <Container className="p-1  rounded-2 " style={{ position: "relative" }}>
       <Card style={{ width: "100%", height: "25rem" }}>
@@ -14,23 +24,28 @@ const DepertmentsCard = ({ image, title, desc, cities, years }) => {
           src={image}
           className="relative"
         />
-       <Link to="/login"> 
-          <button
+        <button
           className=" h-20 p-1 px-2 bg-light  rounded-3 border-0 d-flex flex-nowrap"
           style={{ position: "absolute", top: "190px", right: "10px" }}
+          variant="light"
+          size="lg"
+      
+          // style={{ position: "absolute", top: "190px", right: "10px" }}
+          // className="btn btn-outline-light my-4 py-2 px-4 m-1 flex-wrap"
+          type="submit"
+          // variant="info"
+          // variant="primary"
+          // className="btn btn-outline-light my-2 py-2 m-1 flex-wrap"
+          onClick={toggleShowSignInCompareModal}
         >
           <i
-
-            className="fa-solid fa-rotate-right fa-spin fa-md p-1 "
+            className="fa-solid p-1 fa-rotate-right fa-md"
             style={{ color: "blue" }}
-          >
-            
-          </i>
+          ></i>
           <span className="mx-1"></span>
-          Compare
+          Compare {/* Add icon component here */}
+          {/* <FaSignInAlt /> Sing In */}
         </button>
-       </Link>
-     
         <Card.Body className="d-flex w-100 justify-content-between">
           <div className="left ">
             <Card.Title className="text-start text-primary fs-6">
@@ -45,12 +60,22 @@ const DepertmentsCard = ({ image, title, desc, cities, years }) => {
             </Card.Title>
             <Card.Text className="desc text-start text-muted">{desc}</Card.Text>
           </div>
-          <Link to="/login"> 
-         <div className="h-25 d-inline-flex align-items-center justify-content-center p-2">
-            <i class="bi bi-heart-fill" style={{ color: "blue" }}></i>
+          <div className="h-25 d-inline-flex align-items-center justify-content-center p-2">
+            <Button
+              variant="light"
+              size="lg"
+              // className="h-20 p-1 px-2 rounded-1 border-0 d-flex flex-nowrap"
+              style={{ position: "absolute", top: "190px", right: "10px" }}
+              className="btn btn-outline-light my-5 py-1 px-1 m-1 rounded-1 border-0 d-flex flex-nowrap"
+              type="submit"
+              // variant="info"
+              // variant="primary"
+              // className="btn btn-outline-light my-2 py-2 m-1 flex-wrap"
+              onClick={toggleShowSignInHeartModal}
+            >
+              <i class="bi bi-heart-fill" style={{ color: "blue" }}></i>
+            </Button>
           </div>
-       </Link>
-        
         </Card.Body>
         <ListGroup className="list-group-flush w-100 ">
           <ListGroup.Item>
@@ -70,6 +95,24 @@ const DepertmentsCard = ({ image, title, desc, cities, years }) => {
           </ListGroup.Item>
         </ListGroup>
       </Card>
+            {/* Modal componentleri tanımlanıyor */}
+            <Modal
+        show={showSignInCompareModal}
+        onHide={toggleShowSignInCompareModal}
+        centered
+      >
+        {/* LogIn componenti modalin içine yerleştiriliyor */}
+        <LogIn />
+      </Modal>
+      {/* Modal componentleri tanımlanıyor */}
+      <Modal
+        show={showSignInHeartModal}
+        onHide={toggleShowSignInHeartModal}
+        centered
+      >
+        {/* LogIn componenti modalin içine yerleştiriliyor */}
+        <LogIn />
+      </Modal>
     </Container>
   );
 };
