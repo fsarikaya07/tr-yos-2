b2b054c2525ea901bed1c4faabbabdb5d272491a
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-07-20 15:15:25 +0300

    home card update humeyra

--- src/components/HomePage/CardSlider.jsx
+++ src/components/HomePage/CardSlider.jsx
@@ -3,21 +3,21 @@ import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import "../Style/CardSlider.css";
 
-const Slider = () => {
-  const slideImages = [
-    "https://images.pexels.com/photos/3862130/pexels-photo-3862130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/1216589/pexels-photo-1216589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/3861958/pexels-photo-3861958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/2280571/pexels-photo-2280571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  ];
+const Slider = ({images}) => {
+  // const slideImages = [
+  //   "https://images.pexels.com/photos/3862130/pexels-photo-3862130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   "https://images.pexels.com/photos/1216589/pexels-photo-1216589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   "https://images.pexels.com/photos/3861958/pexels-photo-3861958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   "https://images.pexels.com/photos/2280571/pexels-photo-2280571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   "https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  // ];
 
   return (
     <div className="slide-container">
       <Slide
         // infinite={true}
         canSwipe={false}
-        autoplay={true}
+        autoplay={false}
         slidesToShow={1}
         slidesToScroll={1}
         prevArrow={
@@ -35,7 +35,7 @@ const Slider = () => {
             </div>
           }
       >
-        {slideImages.map((image, index) => (
+        {images.map((image, index) => (
           <div key={index} className="each-slide">
             <div
               className="slide-image"
