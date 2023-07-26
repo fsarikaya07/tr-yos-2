3fec09007fce0212108b0a966387e506f7ec588a
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-07-26 17:31:52 +0300

    v4

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,3 +1,139 @@
+// import React, { createContext, useContext, useState, useEffect } from "react";
+// import axios from "axios";
+
+// const YosContext = createContext();
+
+// export function useYosContext() {
+//   return useContext(YosContext);
+// }
+
+// export function YosProvider({ children }) {
+//   const [cities, setCities] = useState([]);
+//   const [universities, setUniversities] = useState([]);
+//   const [departments, setDepartments] = useState([]);
+//   const [card, setCard] = useState([]);
+//   const [sliderImages, setSliderImages] = useState([]);
+
+//   const [compare, setCompare] = useState([]);
+
+//   const [user, setUser] = useState();
+//   const [compareId, setCompareId] = useState();
+//   const loginData = new FormData();
+//   loginData.append("email", "f.sarikaya00@gmail.com");
+//   loginData.append("password", "123456asd");
+
+//   const compareData = new FormData();
+//   compareData.append("id", "");
+
+//   console.log("object", compare);
+
+//   const ids = compare.map((item) => item.id);
+
+//   const [countries, setCountries] = useState([]);
+//   const [selectedCountryId, setSelectedCountryId] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         //Countries -HB-
+//         const responseCountries = await axios.get(
+//           "https://tr-yös.com/api/v1/location/allcountries.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+//         );
+//         setCountries(responseCountries.data);
+
+//         //SliderImages -HB-
+//         const responseUniversities = await axios.get(
+//           "https://tr-yös.com/api/v1/education/alluniversities.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+//         );
+//         setUniversities(responseUniversities.data);
+//         const images = responseUniversities.data.map(
+//           (university) => university.images
+//         );
+//         setSliderImages(images);
+
+//         const responseCard = await axios.get(
+//           "https://tr-yös.com/api/v1/record/alldepartments.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+//         );
+//         setCard(responseCard.data);
+
+//         const responseDepartments = await axios.get(
+//           "https://tr-yös.com/api/v1/record/alldepartments.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+//         );
+//         setDepartments(responseDepartments.data);
+
+//         //User
+//         const responseUser = await axios.post(
+//           "https://tr-yös.com/api/v1/users/login.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a",
+//           loginData,
+//           {
+//             headers: {
+//               "Content-Type": "multipart/form-data",
+//             },
+//           }
+//         );
+//         setUser(responseUser.data);
+
+//         //Compare
+//         const responseCompare = await axios.get(
+//           `https://tr-yös.com/api/v1/users/allcompares.php`,
+//           {
+//             params: {
+//               id: ids.join(","),
+//               token:
+//                 "SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a",
+//             },
+//           }
+//         );
+//         setCompareId(responseCompare.data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   //Cities -HB-
+//   useEffect(() => {
+//     if (selectedCountryId) {
+//       const fetchCitiesByCountry = async () => {
+//         try {
+//           const responseCities = await axios.get(
+//             `https://tr-yös.com/api/v1/location/citiesbycountry.php?country_id=${selectedCountryId}&token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a`
+//           );
+//           setCities(responseCities.data);
+//         } catch (error) {
+//           console.log(error);
+//         }
+//       };
+
+//       fetchCitiesByCountry();
+//     }
+//   }, [selectedCountryId]);
+
+//   const contextValue = {
+//     cities,
+//     universities,
+//     departments,
+//     card,
+//     sliderImages,
+
+//     compare,
+//     setCompare,
+//     user,
+//     setCompareId,
+//     compareId,
+
+//     countries,
+//     setSelectedCountryId,
+//   };
+
+//   return (
+//     <YosContext.Provider value={contextValue}>{children}</YosContext.Provider>
+//   );
+// }
+
+
 import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 
@@ -13,103 +149,66 @@ export function YosProvider({ children }) {
   const [departments, setDepartments] = useState([]);
   const [card, setCard] = useState([]);
   const [sliderImages, setSliderImages] = useState([]);
-
-  const [compare, setCompare] = useState([]);
-
-  const [user, setUser] = useState();
-  const [compareId, setCompareId] = useState();
-  const loginData = new FormData();
-  loginData.append("email", "f.sarikaya00@gmail.com");
-  loginData.append("password", "123456asd");
-
-  const compareData = new FormData();
-  compareData.append("id", "");
-
-  console.log("object", compare);
-
-  const ids = compare.map((item) => item.id);
-
   const [countries, setCountries] = useState([]);
-  const [selectedCountryId, setSelectedCountryId] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        //Countries -HB-
-        const responseCountries = await axios.get(
-          "https://tr-yös.com/api/v1/location/allcountries.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
-        );
-        setCountries(responseCountries.data);
-
-        //SliderImages -HB-
-        const responseUniversities = await axios.get(
-          "https://tr-yös.com/api/v1/education/alluniversities.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
-        );
-        setUniversities(responseUniversities.data);
-        const images = responseUniversities.data.map(
-          (university) => university.images
-        );
-        setSliderImages(images);
-
-        const responseCard = await axios.get(
-          "https://tr-yös.com/api/v1/record/alldepartments.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
-        );
-        setCard(responseCard.data);
-
-        const responseDepartments = await axios.get(
-          "https://tr-yös.com/api/v1/record/alldepartments.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
-        );
-        setDepartments(responseDepartments.data);
-
-        //User
-        const responseUser = await axios.post(
-          "https://tr-yös.com/api/v1/users/login.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a",
-          loginData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-        setUser(responseUser.data);
-
-        //Compare
-        const responseCompare = await axios.get(
-          `https://tr-yös.com/api/v1/users/allcompares.php`,
-          {
-            params: {
-              id: ids.join(","),
-              token:
-                "SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a",
-            },
-          }
-        );
-        setCompareId(responseCompare.data);
-      } catch (error) {
+    axios
+      .get(
+        "https://tr-yös.com/api/v1/location/allcities.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+      )
+      .then((response) => {
+        setCities(response.data);
+      })
+      .catch((error) => {
         console.log(error);
-      }
-    };
+      });
 
-    fetchData();
+    axios
+      .get(
+        "https://tr-yös.com/api/v1/education/alluniversities.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+      )
+      .then((response) => {
+        setUniversities(response.data);
+        const images = response.data.map((university) => university.images);
+        setSliderImages(images);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get(
+        "https://tr-yös.com/api/v1/record/alldepartments.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+      )
+      .then((response) => {
+        setCard(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get(
+        "https://tr-yös.com/api/v1/record/alldepartments.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+      )
+      .then((response) => {
+        setDepartments(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get(
+        "https://tr-yös.com/api/v1/location/allcountries.php?token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a"
+      )
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
-
-  //Cities -HB-
-  useEffect(() => {
-    if (selectedCountryId) {
-      const fetchCitiesByCountry = async () => {
-        try {
-          const responseCities = await axios.get(
-            `https://tr-yös.com/api/v1/location/citiesbycountry.php?country_id=${selectedCountryId}&token=SX2qL5O3ivipPSMIWN8nXnaLWOiy4cEq7UdgZk448T5ZDpT1qbgMIrXVNquP1CWyNAH3JvoEVqnjiyg20a17549275a86d0e835660e56847e87a`
-          );
-          setCities(responseCities.data);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-
-      fetchCitiesByCountry();
-    }
-  }, [selectedCountryId]);
 
   const contextValue = {
     cities,
@@ -117,18 +216,12 @@ export function YosProvider({ children }) {
     departments,
     card,
     sliderImages,
-
-    compare,
-    setCompare,
-    user,
-    setCompareId,
-    compareId,
-
     countries,
-    setSelectedCountryId,
   };
 
   return (
-    <YosContext.Provider value={contextValue}>{children}</YosContext.Provider>
+    <YosContext.Provider value={contextValue}>
+      {children}
+    </YosContext.Provider>
   );
 }
