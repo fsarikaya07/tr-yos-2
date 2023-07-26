bee7aa665c2310ad82fc4bbf6dbdb95ebeb233c8
Canan Ağbal <agbalcanan8069@gmail.com>
2023-07-26 18:51:01 +0300

    animation

--- src/components/Login/Logİn.jsx
+++ src/components/Login/Logİn.jsx
@@ -4,11 +4,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button, Modal } from "react-bootstrap"; // Modal bileşenini import edelim
 import { useAuthContext } from "../../context/AuthContext";
 
-
 const LogIn = () => {
-
-const { login } = useAuthContext();
-const [email, setEmail] = useState("");
+  const { login } = useAuthContext();
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
@@ -18,7 +16,6 @@ const [email, setEmail] = useState("");
     const isLogined = await login({
       email: email,
       password: password,
-     
     });
     if (isLogined) {
       setEmail("");
@@ -30,32 +27,30 @@ const [email, setEmail] = useState("");
     }
   };
 
-    // const isMessage = await message({
-    //   name: name,
-    //   email: email,
-     
-    // });
-    // const isSuccess = await success({
-    //   name: name,
-    //   email: email,
-     
-    // });
+  // const isMessage = await message({
+  //   name: name,
+  //   email: email,
 
-    // Kayıt başarılıysa yönlendir
- 
+  // });
+  // const isSuccess = await success({
+  //   name: name,
+  //   email: email,
 
+  // });
 
- const [showSignInModal, setShowSignInModal] = useState(false);
+  // Kayıt başarılıysa yönlendir
+
+  const [showSignInModal, setShowSignInModal] = useState(false);
 
   const toggleShowSignInModal = () => setShowSignInModal(!showSignInModal);
 
-// const location =useLocation()
+  // const location =useLocation()
 
   return (
-    <div className="container  d-flex flex-column align-items-center mt-5   col-4 h-75  login-container center">
+    <div className="container box d-flex flex-column align-items-center mt-5   col-4 h-75  login-container center">
       <h2 className="mt-4">Sing In</h2>
       <form
-        className=" py-5  px-3 w-100 h-100 position-relative "
+        className=" py-5  px-3 w-100 h-100 login-form "
         onSubmit={(e) => handleSubmit(e)}
       >
         <div className="form-group w-100 mt-3">
@@ -67,7 +62,7 @@ const [email, setEmail] = useState("");
             placeholder="User Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)} // Input alanı değiştiğinde eposta değerini güncelliyoruz.
-               />
+          />
         </div>
         <div className="form-group w-100 mt-3">
           <input
@@ -97,7 +92,7 @@ const [email, setEmail] = useState("");
           <Link
             style={{ textDecoration: "none" }} //   onClick={() => forgotPassword(email)}
           >
-           Forget Password
+            Forget Password
           </Link>
         </div>
 
@@ -105,16 +100,22 @@ const [email, setEmail] = useState("");
           <hr />
           <p>
             Don't have an account yet?
-            {/* <Link to="/Register" className="text-primary" style={{ textDecoration: 'none' }}> Sign Up</Link> */}
-            <Button
+            <Link
+              to="/Register"
+              className="text-primary"
+              style={{ textDecoration: "none" }}
+            >
+              {" "}
+              Sign Up
+            </Link>
+            {/* <Button
               className="btn btn-outline-light my-4 py-1 px-1 m-1 flex-wrap"
               type="submit"
               variant="info"
-                    onClick={toggleShowSignInModal} // Modalı göstermek için fonksiyonu çağıralım
+              onClick={toggleShowSignInModal} // Modalı göstermek için fonksiyonu çağıralım
             >
-              
               Sing Up
-            </Button>
+            </Button> */}
           </p>
         </div>
         <div className="d-flex justify-content-center align-items-center">
