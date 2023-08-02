72eec3cfa3b5b5607a7030087be6dc629d760840
hacerbulut <hcrleather@gmail.com>
2023-08-02 16:41:26 +0300

    city

--- src/components/university/UniversityCard.jsx
+++ src/components/university/UniversityCard.jsx
@@ -1,9 +1,16 @@
 import "../Style/University.css";
 import { useTranslation } from "react-i18next";
 import { Container } from "react-bootstrap";
+import { useYosContext } from "../../context/Context";
 
 const UniversityCard = ({ logo, city, tr, data }) => {
   const { t } = useTranslation();
+  const { cities } = useYosContext();
+  const citi = cities?.map((e) => {
+    if (e.id == city) {
+      return e.tr;
+    }
+  });
 
   console.log(data);
   return (
@@ -37,16 +44,15 @@ const UniversityCard = ({ logo, city, tr, data }) => {
                 <h5 className="university-title">{tr}</h5>
                 <div className="location">
                   <i className="fa-solid fa-location-dot"></i>
-                  <span className="location-name">Afyonkarahisar</span>
+                  <span className="location-name">{citi} </span>
                 </div>
                 <p className="university-address">
-                  Afyon Kocatepe Üniversitesi Rektörlüğü ANS Kampusü Gazlıgöl
-                  Yolu Üzeri AFYONKARAHİSAR
+                 {data?.adress}
                 </p>
                 <div className="contact">
                   <div className="uni-contact p-2 border border-warning-subtle rounded-1">
                     <i className="fa-solid fa-phone"></i>
-                    <span>(272) 218-1195</span>
+                    <span>{data?.phone} </span>
                   </div>
                 </div>
               </div>
